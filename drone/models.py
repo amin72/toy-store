f1625a770731caf01e9c9bf741b5038dbdbd78ea
Amin <=>
2022-08-02 03:01:02 +0430

    add unique constraint to models

--- drone/models.py
+++ drone/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DroneCategory(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     class Meta:
         ordering = (
@@ -14,7 +14,7 @@ class DroneCategory(models.Model):
 
 
 class Drone(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
     drone_category = models.ForeignKey(
         DroneCategory,
         related_name='drones',
@@ -41,7 +41,12 @@ class Pilot(models.Model):
         (FEMALE, 'Female'),
     )
 
-    name = models.CharField(max_length=150, blank=False, default='')
+    name = models.CharField(
+        max_length=150,
+        blank=False,
+        default='',
+        unique=True
+    )
     gender = models.CharField(
         max_length=2,
         choices=GENDER_CHOICES,
