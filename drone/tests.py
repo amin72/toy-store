40570e45eb23ed29ccd7aa8fa91dc5016dc130a2
Amin <=>
2022-08-04 20:47:17 +0430

    test -- ensure we can create a new Pilot and then retrieve it and cannot retrieve the persisted pilot without a token

--- drone/tests.py
+++ drone/tests.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.utils.http import urlencode
 from django.shortcuts import reverse
 
+from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.test import APITestCase
-from drone.models import DroneCategory
+from drone.models import DroneCategory, Pilot
 from drone import views
 from drone.urls import app_name
 
@@ -122,3 +124,71 @@ class DroneCategoryTests(APITestCase):
         
         assert get_response.status_code == status.HTTP_200_OK
         assert get_response.data['name'] == drone_category_name
+
+
+class PilotTests(APITestCase):
+    """Pilot tests"""
+
+    def post_pilot(self, name, gender, races_count):
+        url = get_full_url(views.PilotList.name)
+        print(url)
+        data = {
+            'name': name,
+            'gender': gender,
+            'races_count': races_count,
+        }
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def create_user_and_set_token_credentials(self):
+        user = User.objects.create_user(
+            'user01',
+            'user01@example.com',
+            'user01P4ssw0rD'
+        )
+        token = Token.objects.create(user=user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Token {token.key}'
+        )
+
+    def test_post_and_get_pilot(self):
+        """
+        Ensure we can create a new Pilot and then retrieve it
+        Ensure we cannot retrieve the persisted pilot without a token
+        """
+
+        self.create_user_and_set_token_credentials()
+        new_pilot_name = 'Gaston'
+        new_pilot_gender = Pilot.MALE
+        new_pilot_races_count = 5
+        response = self.post_pilot(
+            new_pilot_name,
+            new_pilot_gender,
+            new_pilot_races_count
+        )
+        
+        print("nPK {0}n".format(Pilot.objects.get().pk))
+        
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Pilot.objects.count() == 1
+        
+        saved_pilot = Pilot.objects.get()
+        
+        assert saved_pilot.name == new_pilot_name
+        assert saved_pilot.gender == new_pilot_gender
+        assert saved_pilot.races_count == new_pilot_races_count
+        
+        url = get_full_url(
+            views.PilotDetail.name,
+            None,
+            {saved_pilot.pk}
+        )
+
+        authorized_get_response = self.client.get(url, format='json')
+        assert authorized_get_response.status_code == status.HTTP_200_OK
+        assert authorized_get_response.data['name'] == new_pilot_name
+
+        # Clean up credentials
+        self.client.credentials()
+        unauthorized_get_response = self.client.get(url, format='json')
+        assert unauthorized_get_response.status_code == status.HTTP_401_UNAUTHORIZED
